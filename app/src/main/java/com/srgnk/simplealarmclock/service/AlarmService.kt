48a303f313da8eb74765845c1b1feb217b53f3f6
E-Srgnk <e.srgnk@gmail.com>
2021-06-16 14:42:56 +0300

    added vibration for alarm.

--- app/src/main/java/com/srgnk/simplealarmclock/service/AlarmService.kt
+++ app/src/main/java/com/srgnk/simplealarmclock/service/AlarmService.kt
@@ -1,14 +1,19 @@
 package com.srgnk.simplealarmclock.service
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import android.os.VibrationEffect
+import android.os.Vibrator
 import com.srgnk.simplealarmclock.R
 
 class AlarmService : Service() {
 
     private var mediaPlayer: MediaPlayer? = null
+    private var vibrator: Vibrator? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -18,13 +23,31 @@ class AlarmService : Service() {
         mediaPlayer = MediaPlayer.create(this, R.raw.ringtone)
         mediaPlayer?.start()
 
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        val pattern = longArrayOf(0, 1500, 3000)
+
+        if (vibrator?.hasVibrator() == true) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator?.vibrate(
+                    VibrationEffect.createWaveform(
+                        pattern,
+                        VibrationEffect.DEFAULT_AMPLITUDE
+                    )
+                )
+            } else {
+                vibrator?.vibrate(pattern, 0)
+            }
+        }
+
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
         mediaPlayer?.stop()
         mediaPlayer?.release()
-        mediaPlayer = null
+
+        vibrator?.cancel()
     }
 }
